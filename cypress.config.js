8f1ca4c495b9fbb24bea7aa612041b36e08f2e12
deno-limo <deniskiplimo4@gmail.com>
2024-01-31 22:54:56 +0300

    json

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://todomvc-app-for-testing.surge.sh'
+    baseUrl: 'https://todomvc-app-for-testing.surge.sh/'
   }
 })
+
 
