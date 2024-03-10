e9f465eb7f9ab09663bd32c022503a1ea1e607db
denolimo <deniskiplimo4@gmail.com>
2024-03-11 00:42:01 +0300

    fixes

--- cypress/e2e/todo mvc todo.cy.js
+++ cypress/e2e/todo mvc todo.cy.js
@@ -8,5 +8,5 @@ describe("filtering", () => {
 it("adds 2 todos", function () {
   cy.get(".new-todo").type("learn testing{enter}").type("be cool{enter}");
 
-  cy.get(".todo-list li").should("have.length", 2);
+  cy.get(".todo-list li").should("have.length", 2).should("be.visible");
 });
