6743f5c716af0866802f1337f63a742c0f92f3bb
denolimo <deniskiplimo4@gmail.com>
2024-03-11 00:39:13 +0300

    fixes

--- cypress/e2e/Todo Mvc filtering.spec.cy.js
+++ cypress/e2e/Todo Mvc filtering.spec.cy.js
@@ -1,38 +1,32 @@
 ///<reference types='cypress'/>
 
-describe('filtering',()=>{
-    beforeEach(()=>{
-        cy.visit('/')
-   
-    cy.get('.new-todo').type('clean room{enter}')
-    cy.get('.new-todo').type('Learn Javescript{enter}')
-    cy.get('.new-todo').type('Use cypress{enter}')
-    cy.get('.new-todo').type('cook{enter}')
-    cy.get('.new-todo').type('Pray{enter}')
+describe("filtering", () => {
+  beforeEach(() => {
+    cy.visit("/");
 
-    cy.get('.todo-list li:nth-child(2) .toggle').click()
-    
-    })
+    cy.get(".new-todo").type("clean room{enter}");
+    cy.get(".new-todo").type("Learn Javescript{enter}");
+    cy.get(".new-todo").type("Use cypress{enter}");
+    cy.get(".new-todo").type("cook{enter}");
+    cy.get(".new-todo").type("Pray{enter}");
+    cy.get(".new-todo").type("Reharse{enter}");
 
-    it('should filter"Active"Todos',()=>{
-        cy.contains('Active').click()
+    cy.get(".todo-list li:nth-child(2) .toggle").click();
+  });
 
-        cy.get('.todo-list li').should('have.length',4)
+  it('should filter"Active"Todos', () => {
+    cy.contains("Active").click();
 
+    cy.get(".todo-list li").should("have.length", 4);
+  });
+  it('should filter"All"Todos', () => {
+    cy.contains("All").click();
 
-    })
-    it('should filter"All"Todos',()=>{
-        cy.contains('All').click()
+    cy.get(".todo-list li").should("have.length", 5);
+  });
+  it('should filter"Complete"Todos', () => {
+    cy.contains("Complete").click();
 
-        cy.get('.todo-list li').should('have.length',5)
-
-
-    })
-    it('should filter"Complete"Todos',()=>{
-        cy.contains('Complete').click()
-
-        cy.get('.todo-list li').should('have.length',1)
-
-
-    })
-})
+    cy.get(".todo-list li").should("have.length", 1);
+  });
+});
